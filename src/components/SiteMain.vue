ab22a22dcb654f025316ddde30d3e71b04b89569
StefanRaluca <raluca.bubulina@yahoo.com>
2024-03-22 15:11:54 +0100

    add sitemain with dinamic

--- src/components/SiteMain.vue
+++ src/components/SiteMain.vue
@@ -8,7 +8,7 @@ export default {
     },
     data() {
         return {
-            products: [
+            productList: [
                 {
                     imgSrc: '/img/1.webp',
                     discount: '-50%',
@@ -71,13 +71,74 @@ export default {
                 }
             ],
 
-
+            hover: new Array(6).fill(false)
         };
+    },
+    methods: {
+        favoriteProduct(product) {
+            product.favorite = !product.favorite;
+        },
     }
 }
 </script>
 
 <template>
+    <div>
+        <!-- Prima sezione di main  -->
+        <div class="container_main">
+            <div v-for="(product, i) in productList.slice(0, 3)" class="container_products">
+                <section class="products">
+                    <div class="product" @mouseenter="hover[i] = true" @mouseleave="hover[i] = false">
+                        <div class="image-container">
+                            <img :src="product.imgSrc" alt="">
+                            <img class="display_hover" :src="product.imgSrc.replace('.webp', 'b.webp')" alt=""
+                                v-if="hover[i]">
+                        </div>
+                        <div v-if="product.discount" class="discount">{{ product.discount }}</div>
+                        <div v-if="product.sostenibilita" class="Sostenibilità">{{ product.sostenibilita }}</div>
+                        <div class="hearts" @click="favoriteProduct(product)">
+                            <span
+                                :class="{ 'fas fa-heart': product.favorite, 'far fa-heart': !product.favorite, 'red-heart': product.favorite, 'heartColor': !product.favorite }"></span>
+                        </div>
+                    </div>
+                    <div>
+                        <span>{{ product.brand }}</span>
+                        <h5>{{ product.title }}</h5>
+                        <span class="price_now">{{ product.priceNow }}</span>
+                        <span class="price_before">{{ product.priceBefore }}</span>
+                    </div>
+                </section>
+            </div>
+        </div>
+
+        <!-- Seconda sezione di main  -->
+        <div class="container_main">
+            <div v-for="(product, i) in products.slice(3)" class="container_products">
+                <section class="products">
+                    <div class="product" @mouseenter="hover[i + 3] = true" @mouseleave="hover[i + 3] = false">
+                        <div class="image-container">
+                            <img :src="product.imgSrc" alt="">
+                            <img class="display_hover" :src="product.imgSrc.replace('.webp', 'b.webp')" alt=""
+                                v-if="hover[i + 3]">
+                        </div>
+                        <div v-if="product.discount" class="discount">{{ product.discount }}</div>
+                        <div v-if="product.sostenibilita" class="Sostenibilità">{{ product.sostenibilita }}</div>
+                        <div class="hearts" @click="favoriteProduct(product)">
+                            <span
+                                :class="{ 'fas fa-heart': product.favorite, 'far fa-heart': !product.favorite, 'red-heart': product.favorite, 'heartColor': !product.favorite }"></span>
+                        </div>
+
+                    </div>
+                    <div>
+                        <span>{{ product.brand }}</span>
+                        <h5>{{ product.title }}</h5>
+                        <span class="price_now">{{ product.priceNow }}</span>
+                        <span class="price_before">{{ product.priceBefore }}</span>
+                    </div>
+                </section>
+            </div>
+        </div>
+    </div>
 
 </template>
 
